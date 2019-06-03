a687c61d7e6e8070452cc4e3c17a9d768357f3c4
Aldy <aldycool@gmail.com>
2019-06-04 00:48:55 +0700

    Initial

---
+++ NETCoreSyncMobileSample/NETCoreSyncMobileSample/Views/NewItemPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using NETCoreSyncMobileSample.Models;
+
+namespace NETCoreSyncMobileSample.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+
+            Item = new Item
+            {
+                Text = "Item name",
+                Description = "This is an item description."
+            };
+
+            BindingContext = this;
+        }
+
+        async void Save_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Send(this, "AddItem", Item);
+            await Navigation.PopModalAsync();
+        }
+
+        async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
